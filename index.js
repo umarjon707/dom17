a075715bf3eda6b5df2be89506ee86a931bc3bbc
Umarjon <urr777777@gmail.com>
2024-06-20 17:18:55 +0500

    fixed more time

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
-const place = document.querySelector('.container')
-const sorted = arr.toSorted((a,b) => a.price - b.price)
-const five = document.querySelector('.five')
-const all = document.querySelector('.all')
+
+//     const component = Item(item)
+//     place.append(component)
+//     function showitems(itemsshow) {
+//         five.onclick = () =>{
+//             const fiveitems = sorted.slice(0, 5);
+//             itemsshow(fiveitems)
+//         }
+//         all.onclick = () =>{
+//             itemsshow(sorted)
+//         }
+//         itemsshow(sorted)
+//     }
+
+
+
+
+// }    
+
+const place = document.querySelector('.container');
+const sorted = arr.sort((a, b) => a.price - b.price); // Use sort() 
+const five = document.querySelector('.five');
+const all = document.querySelector('.all');
+
+function showItems(itemsToShow) {
+  itemsToShow.forEach(item => {
+    const component = Item(item);
+    place.append(component);
+  });
+}
+
+showItems(sorted);
+
+five.onclick=()=>{
+    showItems(sorted.slice(0, 5));
+}
+  
+all.omclick = () =>{
+    showItems(sorted);
+}
+ 
 for (let item of sorted) {
-    const component = Item(item)
-    place.append(component)
-    function showitems(itemsshow) {
-        five.onclick = () =>{
-            const fiveitems = sorted.slice(0, 5);
-            itemsshow(fiveitems)
-        }
-        all.onclick = () =>{
-            itemsshow(sorted)
-        }
-        itemsshow(sorted)
-    }
-
-
-
-
-}    
-
+  const component = Item(item);
+  place.append(component);
+}
